9ba7ec5d337bf1d484f961abf0d2255f18b50244
CubeStorm <marczelloweb@gmail.com>
2020-07-05 19:00:51 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -49,16 +49,16 @@ var game = {
         if (jumpVar == false) {
             game.jumpVerify();
             for (let i = 1; i < 10; i++) {
-                setTimeout(game.clear, i*30);
-                setTimeout(game.up, i*30);
-                setTimeout(reload, i*30);
+                setTimeout(game.clear, i*20);
+                setTimeout(game.up, i*20);
+                setTimeout(reload, i*20);
             }
             for (let i = 1; i < 10; i++) {
-                setTimeout(game.clear, i*30 + 400);
-                setTimeout(game.down, i*30 + 400);
-                setTimeout(reload, i*30 + 400);
+                setTimeout(game.clear, i*20 + 400);
+                setTimeout(game.down, i*20 + 400);
+                setTimeout(reload, i*20 + 400);
             }
-            setTimeout(game.jumpVerify, 10*40 + 300);
+            setTimeout(game.jumpVerify, 10*20 + 300);
         }
     },
     floor: function() { // Rysowanie podłogi
